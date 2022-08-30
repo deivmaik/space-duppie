0263b11a560cbad0ce99e400c1d2793e9bb16522
deivmaik <deivmaik@users.noreply.github.com>
2022-08-30 13:09:15 +0200

    Add sprites, animations and configurate jump

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    //Player movement variables
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+
+        // Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
+    }
+    
+    void FixedUpdate()
+    {
+        if (rigidBody.velocity.x < runningSpeed){
+            rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if(IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    // Method indicates if the player is touching the floor or not
+    bool IsTouchingTheGround()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 2.0f, groundMask)){
+            //TODO: program logic of contact with the floor
+            return true;
+        } else {
+            //TODO: program no contact logic
+            return false;
+        }
+    }
+}
